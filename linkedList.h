d39cdb5cf2658f8e017e28a17b5903e1b426d6d9
akumbhakar <144550921+akumbhakar@users.noreply.github.com>
2023-09-15 07:05:51 +0530

    Add files via upload

---
+++ linkedList.h
@@ -0,0 +1,26 @@
+#ifndef _LIST_H
+#define _LIST_H
+
+struct consumer{
+    char *consumer_ID;
+    char *name;
+    char *aadhar_ID;
+    char *DOB;
+};
+
+struct node{
+    struct consumer customer;
+    struct node *next;
+
+}; 
+
+int nodeCount(struct node *copy_head);
+int insert_node_at_end(struct node **head, struct consumer customer);
+int delete_list(struct node **head);
+int insert_node_at_head(struct node **head,struct consumer customer);
+int search_node(struct node *copy_head, char* customer_ID);
+int print_node(struct node *copy_head);
+int delete_node(struct node **head,char* customer_ID);
+
+
+#endif
