86e0db674ffda80b2eeb28fd956011322f013866
akumbhakar <144550921+akumbhakar@users.noreply.github.com>
2023-09-18 08:47:55 +0530

    Add files via upload
    Test c file to test my linked list

--- mark2.c
+++ mark2.c
@@ -3,20 +3,20 @@
 #include "linkedList.h"
 
 int main(){
-    
+    int index;
     struct node *head = NULL;
-    struct consumer customer1 = {"2130564528","GOBARDHAN KUMBHAKAR","123423561254","12/06/1950"};
-    struct consumer customer2 = {"2130562536","MOUSUMI KUMBHAKAR","523156897854","02/05/1992"};
-    struct consumer customer3 = {"2130564525","BAISHAKHI KUMBHAKAR","456823598659","02/05/1994"};
-    struct consumer customer4 = {"2130568954","AKASH KUMBHAKAR","852434267065","18/06/2001"};
-    struct consumer customer5 = {"2150569653","PRAKASH KUMBHAKAR","865945215632","30/12/2012"};
+    struct consumer customer[] = {  {"2130564528","GOBARDHAN KUMBHAKAR","123423561254","12/06/1950"},
+                                    {"2130562536","MOUSUMI KUMBHAKAR","523156897854","02/05/1992"},
+                                    {"2130564525","BAISHAKHI KUMBHAKAR","456823598659","02/05/1994"},
+                                    {"2130568954","AKASH KUMBHAKAR","852434267065","18/06/2001"},
+                                    {"2150569653","PRAKASH KUMBHAKAR","865945215632","30/12/2012"}
+    
+    };
 
 
-    insert_node_at_end(&head,customer1);
-    insert_node_at_end(&head,customer2);
-    insert_node_at_end(&head,customer3);
-    insert_node_at_end(&head,customer5);
-    insert_node_at_head(&head,customer4);
+    for(int i = 0; i < (sizeof(customer)/sizeof(customer[0])); i++){
+        insert_node_at_end(&head,customer[i]);
+    }
 
     struct node* current = head;
     while(current != NULL){
@@ -25,18 +25,16 @@ int main(){
         current = current->next;
     }
 
-    search_node(head,"2130568954");
-    printf(".......................................\n");
-    delete_node(&head,"2130568952");
-    
+    printf("Enter Index you want to delete:: ");
+    scanf("%d",&index);
+    delete_at_index(&head,index);
+    printf("\n$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n");
     current = head;
     while(current != NULL){
         print_node(current);
 
         current = current->next;
     }
-    
-    
     
     delete_list(&head);
     
