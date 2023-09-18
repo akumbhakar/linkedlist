69a837fa9dd3b6c0a14103cd24bcec62345ce6c1
akumbhakar <144550921+akumbhakar@users.noreply.github.com>
2023-09-18 08:43:42 +0530

    Add files via upload

--- listImp.c
+++ listImp.c
@@ -52,6 +52,7 @@ int insert_node_at_end(struct node **head, struct consumer customer){
         printf("Newnode assigned successfully.\n\n");
     }
     
+    return 0;
     
     
 
@@ -106,15 +107,16 @@ int print_node(struct node *copy_head){
         This funtion print each node by taking its address if it is not NULL
     */
     if(copy_head != NULL){
-        printf("Customer ID: %s\n",copy_head->customer.consumer_ID);
-        printf("Name: %s\n",copy_head->customer.name);
-        printf("Addahar ID: %s\n",copy_head->customer.aadhar_ID);
-        printf("DOB: %s\n",copy_head->customer.DOB);
+        printf("    Customer ID: %s\n",copy_head->customer.consumer_ID);
+        printf("    Name: %s\n",copy_head->customer.name);
+        printf("    Addahar ID: %s\n",copy_head->customer.aadhar_ID);
+        printf("    DOB: %s\n",copy_head->customer.DOB);
         printf("----------------------------------------------------------------\n");
     }
     else{
         printf("Can not print NULL pointer!!!\n");
     }
+    return 0;
 }
 
 int search_node(struct node *copy_head, char* customer_ID){
@@ -171,4 +173,67 @@ int delete_node(struct node **head,char* customer_ID){
 
     printf("Customer not exist!!\n");
     printf("......................................................\n");
+    return 0;
 }
+
+int insert_at_index(struct node **head, int index, struct consumer customer){
+    /*
+        This funtion work as 1 based index....
+        [index 1]->[index 2]->[index 3]->[index 4]-> NULL
+    */
+    struct node *tempo = *head;
+    struct node *holder = NULL;
+    int count = 0;
+    int p_count = nodeCount(tempo);
+
+    if(index > p_count || index <= 1){
+        printf("Index out of range!!:(\n");
+        return 0;
+    }
+    struct node *newnode = (struct node*)malloc(sizeof(struct node));
+    newnode->customer = customer;
+    newnode->next = NULL;
+    
+    while(tempo != NULL && count < (index - 1)){
+        count = count + 1;
+        holder = tempo;
+        tempo = tempo->next;
+    }
+    holder->next = newnode;
+    newnode->next = tempo;
+
+    if(nodeCount(*head) == p_count){
+        free(newnode);
+        return 0;
+    }
+
+
+}
+
+int delete_at_index(struct node **head, int index){
+    /*
+        This funtion work as 1 based index..
+        [index 1]->[index 2]->[index 3]->[index 4]-> NULL
+    */
+    struct node *tempo = *head;
+    struct node *holder = NULL;
+    int count = 0;
+    int p_count = nodeCount(tempo);
+    if(index >p_count || index <= 1){
+        printf("Index out of range!!:(");
+        return 0;
+    }
+    while(tempo != NULL && count < (index - 1)){
+        count = count + 1;
+        holder = tempo;
+        tempo = tempo->next;
+    }
+    struct node *auxiliary = tempo->next;
+    free(holder->next);
+    holder->next = auxiliary;
+
+
+
+}
+
+
